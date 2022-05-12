089054b9931a572fd4df196e1db7f8296a9fc1af
Vinicius Redorat <viniciusredorat@gmail.com>
2022-05-11 22:03:00 -0300

    Atividade final 11_05_2022

---
+++ Par_ou_Impar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Par_ou_Impar
+{
+    static void Main (string[] args)
+    {
+        string nome;
+        int numero, contador = 0;
+
+        Console.WriteLine("Digite seu nome");
+        nome = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine(nome + " Digie um valor inteiro");
+        numero = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        while (contador <= numero)
+        {
+            if (contador % 2 == 0)
+            {
+                Console.WriteLine("{0} é par", contador);
+            }
+            else
+            {
+                Console.WriteLine("{0} é impar", contador);
+            }
+            contador++;           
+        } 
+    }
+}
